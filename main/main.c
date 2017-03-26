7eab1a48307e5c31af06afdaebad484732674534
Quinlan Pfiffer <qpfiffer@gmail.com>
2017-03-26 11:48:18 -0700

    Print out SSIDs from Beacon frames.

--- main/main.c
+++ main/main.c
@@ -9,6 +9,7 @@
 #include "esp_wifi_types.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "string.h"
 
 typedef struct  __attribute__((__packed__)) {
 	unsigned protocol_version:2;
@@ -20,10 +21,27 @@ typedef struct  __attribute__((__packed__)) {
 typedef struct __attribute__((__packed__)) {
 	wifi_pkt_frame_ctrl_t frame_ctrl;
 	uint8_t duration[2];
-	uint8_t addr_1[6];
-	uint8_t addr_2[6];
-	uint8_t addr_3[6];
+	uint8_t reciever[6];
+	uint8_t transmitter[6];
+	uint8_t bssid[6];
+	uint8_t seq_ctrl[2];
+	uint8_t payload[0]; /* Pointer to start of payload area. */
 } wifi_pkt_header_t;
+
+typedef struct __attribute__((__packed__)) {
+	uint8_t element_id;
+	uint8_t len;
+	uint8_t ssid[32]; /* Maximum of 32. */
+
+} wifi_pkt_beacon_ssid_t;
+
+// From here: https://mrncciew.com/2014/10/08/802-11-mgmt-beacon-frame/
+typedef struct __attribute__((__packed__)) {
+	uint8_t timestamp[8];
+	uint8_t beacon_interval[2];
+	uint8_t capability_interval[2];
+	wifi_pkt_beacon_ssid_t ssid; /* 0-32 bytes. wat. */
+} wifi_pkt_beacon_payload_t;
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
@@ -42,10 +60,15 @@ void process_promisc(void* buf, wifi_promiscuous_pkt_type_t type)
 			break;
 	}
 	const wifi_promiscuous_pkt_t *packet = buf;
-	//const unsigned int timestamp = packet->rx_ctrl.timestamp;
 	//const unsigned int packet_siz = packet->rx_ctrl.sig_len;
+	//const unsigned int timestamp = packet->rx_ctrl.timestamp;
 
 	const wifi_pkt_header_t *header = (wifi_pkt_header_t *)packet->payload;
+
+	const uint8_t *addr_1 = header->reciever;
+	const uint8_t *addr_2 = header->transmitter;
+	printf("Reciever: %02X:%02X:%02X:%02X:%02X:%02X\n", addr_1[0], addr_1[1], addr_1[2], addr_1[3], addr_1[4], addr_1[5]);
+	printf("Transmitter: %02X:%02X:%02X:%02X:%02X:%02X\n", addr_2[0], addr_2[1], addr_2[2], addr_2[3], addr_2[4], addr_2[5]);
 
 	// Offsets from here:
 	// https://www.savvius.com/resources/compendium/wireless_lan/wlan_packet_types
@@ -53,14 +76,19 @@ void process_promisc(void* buf, wifi_promiscuous_pkt_type_t type)
 		printf("Probe request packet.\n");
 	else if (header->frame_ctrl.subtype & 0x5)
 		printf("Probe response packet.\n");
-	else if (header->frame_ctrl.subtype & 0x8)
+	else if (header->frame_ctrl.subtype & 0x8) {
 		printf("Beacon packet.\n");
+		const wifi_pkt_beacon_payload_t *beacon_payload = (wifi_pkt_beacon_payload_t *)header->payload;
+		printf("SSID Length: %d\n", beacon_payload->ssid.len);
+		char ssid_buf[32] = {0};
+		const size_t ssid_len = (size_t)beacon_payload->ssid.len;
+		const size_t copy_num = ssid_len > sizeof(ssid_buf) ? sizeof(ssid_buf) : ssid_len;
 
+		memcpy(ssid_buf, beacon_payload->ssid.ssid, copy_num);
+		ssid_buf[copy_num] = '\0';
 
-	const uint8_t *addr_1 = header->addr_1;
-	const uint8_t *addr_2 = header->addr_2;
-	printf("Addr1: %02X:%02X:%02X:%02X:%02X:%02X\n", addr_1[0], addr_1[1], addr_1[2], addr_1[3], addr_1[4], addr_1[5]);
-	printf("Addr2: %02X:%02X:%02X:%02X:%02X:%02X\n", addr_2[0], addr_2[1], addr_2[2], addr_2[3], addr_2[4], addr_2[5]);
+		printf("SSID(?): %s\n", ssid_buf);
+	}
 }
 
 void app_main(void)
